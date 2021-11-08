9df829d5bfe3e569ed7991e934a985ed86f3b81c
hayahann <hhayashi@calpoly.edu>
2021-11-08 07:21:11 -0800

    fixed warnings

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar';
 import './HomePage.css';
-//import '../index.css';
 
 function HomePage() {
 
