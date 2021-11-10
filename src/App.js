96105997c3b81c47ce66b23ea3035b8d78e86fba
hayahann <hhayashi@calpoly.edu>
2021-11-10 15:20:27 -0800

    fixed warnings app.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ import PlaylistPage from './pages/PlaylistPage';
 
 function MyApp() {
 
-    var client_id = '5f8ac560aa6a4deca706bde30ad28531'; //'CLIENT_ID'; // Your client id
-    var client_secret = 'fa9822db14bb4a6595d4486024a8eec0';//CLIENT_SECRET'; // Your secret
-    var redirect_uri = 'http://songbank.com/callback/';//'REDIRECT_URI'; // Your redirect uri
-
     return (
         <>
         <BrowserRouter>
