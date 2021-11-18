f5305f9f9e04af4003872c03b452d5ebdee1e6c2
heberman <71403435+heberman@users.noreply.github.com>
2021-11-17 21:44:26 -0800

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,249 @@
+const express = require('express');
+const cors = require('cors');
+
+const songServices = require('./song-services');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+app.listen(process.env.PORT || port, () => {
+    console.log("REST API is listening.");
+  });
+
+const playlists = {
+    playlist_list : []
+}
+
+const songs = {
+    song_list :
+    [
+        {
+            title: 'Easy On Me',
+            Artist: 'Adele',
+            runtime: 3.45,
+        },
+        {
+            title: 'Shivers',
+            Artist: 'Ed Sheeran',
+            runtime: 3.28,
+        },
+        {
+            title: 'MONEY',
+            Artist: 'LISA',
+            runtime: 2.48,
+        },
+        {
+            title: 'STAY',
+            Artist: 'The Kid LAROI',
+            featured: 'Justin Bieber',
+            runtime: 2.22,
+        },
+        {
+            title: 'INDUSTRY BABY',
+            Artist: 'Lil Nas X',
+            featured: 'Jack Harlow',
+            runtime: 3.32,
+        },
+        {
+            title: 'Heat Waves',
+            Artist: 'Glass Animals',
+            runtime: 3.59,
+        },
+        {
+            title: 'My Universe',
+            Artist: 'Coldplay',
+            featured: 'BTS',
+            runtime: 3.48,
+        },
+        {
+            title: 'love nwantiti (ah ah ah)',
+            Artist: 'CKay',
+            runtime: 2.26,
+        },
+        {
+            title: 'Woman',
+            Artist: 'Doja Cat',
+            runtime: 2.53,
+        },
+        {
+            title: 'Cold Heart - PNAU Remix',
+            Artist: 'Elton John',
+            featured: 'Dua Lipa',
+            runtime: 3.23,
+        },
+        {
+            title: 'THATS WHAT I WANT',
+            Artist: 'Lil Nas X',
+            runtime: 2.24,
+        },
+        {
+            title: 'Bad Habits',
+            Artist: 'Ed Sheeran',
+            runtime: 3.51,
+        },
+        {
+            title: 'Moth To A Flame',
+            Artist: 'Swedish House Mafia',
+            runtime: 3.54,
+        },
+        {
+            title: 'Ghost',
+            Artist: 'Justin Bieber',
+            runtime: 2.33,
+        },
+        {
+            title: 'Happier Than Ever',
+            Artist: 'Billie Eilish',
+            runtime: 2.31,
+        },
+        {
+            title: 'good 4 u',
+            Artist: 'Olivia Rodrigo',
+            runtime: 2.58,
+        },
+        {
+            title: 'Need to Know',
+            Artist: 'Doja Cat',
+            runtime: 3.31,
+        },
+        {
+            title: 'MONTERO(Call Me By Your Name',
+            Artist: 'Lil Nas X',
+            runtime: 2.18,
+        },
+        {
+            title: 'Life Goes On',
+            Artist: 'Oliver Tree',
+            runtime: 2.42,
+        },
+        {
+            title: 'Pepas',
+            Artist: 'Farruko',
+            runtime: 4.47,
+        },
+        {
+            title: "Don't Be Shy",
+            Artist: 'Tiesto',
+            featured: 'KAROL G',
+            runtime: 2.21,
+        },
+        {
+            title: 'Let Somebody Go',
+            Artist: 'Coldplay',
+            featured: 'Selena Gomez',
+            runtime: 4.02,
+        },
+        {
+            title: 'Take My Breath',
+            Artist: 'The Weeknd',
+            runtime: 3.40,
+        },
+        {
+            title: 'Kiss Me More',
+            Artist: 'Doja Cat',
+            featured: 'SZA',
+            runtime: 3.29,
+        },
+        {
+            title: 'Levitating',
+            Artist: 'Dua Lipa',
+            featured: 'DaBaby',
+            runtime: 3.23,
+        },
+        {
+            title: 'OUT OUT',
+            Artist: 'Joel Corry',
+            featured: 'Jax Jones',
+            runtime: 2.43,
+        },
+        {
+            title: 'Meet Me At Our Spot',
+            Artist: 'THE ANXIETY',
+            featured: 'Willow',
+            runtime: 2.43,
+        },
+        {
+            title: 'Livin It Up',
+            Artist: 'Young Thug',
+            featured: 'Post Malone',
+            runtime: 3.31,
+        },
+        {
+            title: 'traitor',
+            Artist: 'Olivia Rodrigo',
+            runtime: 3.49,
+        },
+        {
+            title: 'Where Are You Now',
+            Artist: 'Lost Frequencies',
+            featured: 'Calum Scott',
+            runtime: 2.28,
+        },
+    ]
+}
+
+app.get('/songs', async (req, res) => {
+    const title = req.query['title'];
+    if (title !== undefined) {
+        const result = await songServices.findSongByTitle(title);
+        if (result === undefined || result === null)
+            res.status(404).send('Resource not found.');
+        else {
+            res.send(result);
+        }
+    } else {
+        const result = await songServices.getSongs();
+        res.send(result);
+    }
+});
+
+app.get('/playlists', async (req, res) => {
+    const title = req.query['title'];
+    if (title !== undefined) {
+        const result = await songServices.findPlaylistByTitle(title);
+        if (result === undefined || result === null)
+            res.status(404).send('Resource not found.');
+        else {
+            res.send(result);
+        }
+    } else {
+        const result = await songServices.getPlaylists();
+        res.send(result);
+    }
+});
+
+app.post('/playlists', (req, res) => {
+    const playlistToAdd = req.body;
+    songServices.addPlaylist(playlistToAdd);
+    res.status(201).send(playlistToAdd).end();
+});
+
+app.delete('/playlists/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = songServices.findPlaylistById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        songServices.deletePlaylist(id)
+        res.status(204).end();
+    }
+});
+
+app.put('/playlists/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = songServices.findPlaylistById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        const newPlaylist = req.body;
+        songServices.editPlaylist(id, newPlaylist);
+        res.status(204).end();
+    }
+});
---
+++ song-services.js
@@ -0,0 +1,135 @@
+const mongoose = require('mongoose');
+const SongSchema = require("./song");
+const PlaylistSchema = require("./playlist");
+const process = require("process");
+const dotenv = require("dotenv");
+dotenv.config();
+
+let conn;
+
+function setConnection(newConn) {
+    return (conn = newConn);
+}
+
+function getConnection() {
+    if (!conn) {
+        if (process.argv.includes("--prod")) {
+            conn = mongoose.createConnection(
+                "mongodb+srv://" +
+                process.env.MONGO_USER +
+                ":" +
+                process.env.MONGO_PWD +
+                "@songs.pax5y.mongodb.net/" +
+                process.env.MONGO_DB +
+                "?retryWrites=true&w=majority",
+                {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                }
+            );
+        } else {
+            conn = mongoose.createConnection("mongodb://localhost:27017/songs", {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+        }
+    }
+    return conn;
+}
+
+// async function getUsers(name, job){
+//     let result;
+//     if (name === undefined && job === undefined){
+//         result = await userModel.find();
+//     }
+//     else if (name && (job === undefined)) {
+//         result = await findUserByName(name);
+//     }
+//     else if (job && (name === undefined)){
+//         result = await findUserByJob(job);
+//     }
+//     else {
+//         result = await findUserByNameAndJob(name, job);
+//     }
+//     return result;
+// }
+//
+// async function findUserById(id){
+//     try{
+//         return await userModel.findById(id);
+//     }catch(error) {
+//         console.log(error);
+//         return undefined;
+//     }
+// }
+
+async function addPlaylist(playlist){
+    try{
+        const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+        const playlistToAdd = new playlistModel(playlist);
+        return await playlistToAdd.save();
+    }catch(error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function deletePlaylist(id){
+    try{
+        const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+        return await playlistModel.findByIdAndDelete(id);
+    }catch(error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function editPlaylist(id, newPlaylist){
+    try{
+        const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+        return await playlistModel.findByIdAndUpdate(id, newPlaylist);
+    }catch(error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function getPlaylists() {
+    const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+    return await playlistModel.find();
+}
+
+async function getSongs() {
+    const songModel = getConnection().model("Song", SongSchema);
+    return await songModel.find();
+}
+
+async function findSongByTitle(title){
+    const songModel = getConnection().model("Song", SongSchema);
+    return await songModel.find({'title':title});
+}
+
+async function findPlaylistByTitle(title){
+    const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+    return await playlistModel.find({'title':title});
+}
+
+async function findPlaylistById(id){
+    const playlistModel = getConnection().model("Playlist", PlaylistSchema);
+    return await playlistModel.find({'_id':id});
+}
+
+// async function findUserByJob(job){
+//     return await userModel.find({'job':job});
+// }
+
+// exports.getUsers = getUsers;
+exports.findSongByTitle = findSongByTitle;
+exports.findPlaylistByTitle = findPlaylistByTitle;
+exports.findPlaylistById = findPlaylistById;
+exports.getPlaylists = getPlaylists;
+exports.getSongs = getSongs;
+exports.setConnection = setConnection;
+exports.addPlaylist = addPlaylist;
+exports.deletePlaylist = deletePlaylist;
+exports.editPlaylist = editPlaylist;
