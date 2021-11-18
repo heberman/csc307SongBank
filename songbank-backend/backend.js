77f7092c2ec05c4eee0786eb0c22ffec8abed56a
heberman <71403435+heberman@users.noreply.github.com>
2021-11-17 21:45:12 -0800

    Add files via upload

--- songbank-backend/backend.js
+++ songbank-backend/backend.js
@@ -191,10 +191,15 @@ const songs = {
 
 app.get('/songs', async (req, res) => {
     const title = req.query['title'];
-    const result = await songServices.findSongByTitle(title);
-    if (result === undefined || result === null)
-        res.status(404).send('Resource not found.');
-    else {
+    if (title !== undefined) {
+        const result = await songServices.findSongByTitle(title);
+        if (result === undefined || result === null)
+            res.status(404).send('Resource not found.');
+        else {
+            res.send(result);
+        }
+    } else {
+        const result = await songServices.getSongs();
         res.send(result);
     }
 });
@@ -217,14 +222,28 @@ app.get('/playlists', async (req, res) => {
 app.post('/playlists', (req, res) => {
     const playlistToAdd = req.body;
     songServices.addPlaylist(playlistToAdd);
-    addPlaylistLocally(playlistToAdd);
     res.status(201).send(playlistToAdd).end();
 });
 
-function addPlaylistLocally(playlist){
-    playlists['playlist_list'].push(playlist);
-}
+app.delete('/playlists/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = songServices.findPlaylistById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        songServices.deletePlaylist(id)
+        res.status(204).end();
+    }
+});
 
-// const findSongByTitle = (title) => {
-//     return songs['song_list'].filter( (song) => song['title'] === title);
-// }
+app.put('/playlists/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = songServices.findPlaylistById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        const newPlaylist = req.body;
+        songServices.editPlaylist(id, newPlaylist);
+        res.status(204).end();
+    }
+});
