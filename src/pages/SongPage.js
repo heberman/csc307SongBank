2f1460855e8994870cd07f3cd4d12fe3fe805275
reedmccullough <reed.m.mccullough@gmail.com>
2021-12-06 00:09:19 -0800

    fixed merging issue

--- src/pages/SongPage.js
+++ src/pages/SongPage.js
@@ -1,68 +1,3 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import logo from "./logo.png"
-
-
-function SongPage() {
-
-    let { title } = useParams();
-    let { artist } = useParams();
-    let { album } = useParams();
-    const [image, setImage] = useState('');
-    const [player, setPlayer] = useState('');
-
-
-    async function getResults() {
-        const songTitle = title;
-        try {
-            const result = await axios.get("/auth/search/" + songTitle);
-            if (result !== undefined) {
-                const searchImage = (result.data)['tracks']['items'][0]['album']['images'][0]['url'];
-                const searchPlayer = (result.data)['tracks']['items'][0]['preview_url'];
-                //console.log('results'+searchResults);
-                setImage(searchImage);
-                setPlayer(searchPlayer);
-            }
-            else {
-                console.log("Not found.");
-            }
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    function LoadImg(){
-        getResults();
-        console.log("hey there: "+player);
-        return (
-            <>
-            <img src={image} height="275" alt="album_pic"></img>
-            <br></br>
-            <br></br>
-            <audio controls>
-                <source src={player} />
-            </audio>
-            </>
-        );
-    }
-
-    return (
-    <div className="container">
-        <div className="image_pos">
-            <a href="/"><img src={logo} alt="logo" height="70" align="top"/></a>
-        </div>
-        <br></br>
-        <br></br>
-        <h1>{title}</h1>
-        <h3>Artist: { artist }</h3>
-        <p><b>Album: { album }</b></p>
-        <LoadImg/>
-    </div>
-
-     );
-}
 
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
@@ -127,7 +62,7 @@ function SongPage() {
         console.log("hey there: "+player);
         return (
             <>
-                <img src={image} height="275"></img>
+                <img src={image} alt="album_pic"height="275"></img>
                 <br></br>
                 <br></br>
                 <audio controls>
